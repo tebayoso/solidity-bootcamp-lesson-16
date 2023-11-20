5268f9ae5a5f820542bea587bae6ece1f4f945a4
Jorge de los Santos <tebayoso@gmail.com>
2023-11-20 11:07:53 -0300

    Initial commit from create-wagmi

---
+++ src/wagmi.ts
@@ -0,0 +1,28 @@
+import { getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { configureChains, createConfig } from 'wagmi'
+import { goerli, mainnet } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+
+const walletConnectProjectId = 'b567397e7732fe881fbef1d1f5b74144'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
+  [
+    publicProvider(),
+  ],
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'My wagmi + RainbowKit App',
+  chains,
+  projectId: walletConnectProjectId,
+})
+
+export const config = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+})
+
+export { chains }
